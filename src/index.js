1b236a64f387fa49042ec384caf7d58b8ad20e10
Junior Dugue <jrdugue@gmail.com>
2019-10-07 11:15:35 -0400

    created additional component/folders and turned app to class component
    created additional component/folders and turned app to class component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, {Component} from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function App() {
+class App extends Component {
+  render(){
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Hello</h2>
     </div>
   );
+  }
 }
 
 const rootElement = document.getElementById("root");
